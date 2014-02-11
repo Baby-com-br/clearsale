75ebccd4e9a94e2ead1bbe40dba4bcff1e640994
luizhespanha <luiz.hespanha@gmail.com>
2014-02-11 18:41:04 -0200

    adjusting tests to verify the header of the soap request

--- spec/requests/clearsale_spec.rb
+++ spec/requests/clearsale_spec.rb
@@ -4,9 +4,10 @@ require 'clearsale'
 require 'webmock/rspec'
 
 describe 'Risk Analysis with ClearSale' do
+  let!(:vcs_params) { {:match_requests_on => [:headers]} }
   describe 'sending orders' do
     it "returns the package response" do
-      VCR.use_cassette('clearsale_send_orders') do
+      VCR.use_cassette('clearsale_send_orders', vcs_params) do
         response = Clearsale::Analysis.send_order(order, payment, user)
 
         response.should be_manual_analysis
@@ -17,9 +18,10 @@ describe 'Risk Analysis with ClearSale' do
   end
 
   describe "updating order status" do
+    let!(:vcs_params) { {:match_requests_on => [:headers]} }
     context "existing order" do
       it "returns the package response" do
-        VCR.use_cassette('clearsale_get_order_status') do
+        VCR.use_cassette('clearsale_get_order_status', vcs_params) do
           response = Clearsale::Analysis.get_order_status('1234')
           response.should be_manual_analysis
           response.score.should be_within(0.01).of(21.11)
@@ -29,8 +31,9 @@ describe 'Risk Analysis with ClearSale' do
     end
 
     context "missing order" do
+      let!(:vcs_params) { {:match_requests_on => [:headers]} }
       it "returns the package response" do
-        VCR.use_cassette('clearsale_get_order_status_missing') do
+        VCR.use_cassette('clearsale_get_order_status_missing', vcs_params) do
           order = double('Order', :id => 1234567890)
 
           response = Clearsale::Analysis.get_order_status(order)
@@ -41,9 +44,10 @@ describe 'Risk Analysis with ClearSale' do
   end
 
   describe "setting the environment" do
+    let!(:vcs_params) { {:match_requests_on => [:headers]} }
     context "when CLEARSALE_ENV is production" do
       it "should access production url" do
-        VCR.use_cassette('clearsale_get_order_status_production') do
+        VCR.use_cassette('clearsale_get_order_status_production', vcs_params) do
           Clearsale::Analysis.clear_connector
           ENV["CLEARSALE_ENV"] = 'production'
           Clearsale::Analysis.get_order_status('1234')
@@ -54,8 +58,9 @@ describe 'Risk Analysis with ClearSale' do
     end
 
     context "when CLEARSALE_ENV isn't production" do
+      let!(:vcs_params) { {:match_requests_on => [:headers]} }
       it "should access production url" do
-        VCR.use_cassette('clearsale_get_order_status') do
+        VCR.use_cassette('clearsale_get_order_status', vcs_params) do
           Clearsale::Analysis.clear_connector
           ENV["CLEARSALE_ENV"] = 'any'
           Clearsale::Analysis.get_order_status('1234')
